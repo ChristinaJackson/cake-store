0e190a92e04272df58ae8cf99fe743d5fc73c0a4
Rostyslav <rklyba@yahoo.com>
2020-05-05 21:28:15 +0300

    Added more JS logic, added product.json file

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// When I wrote this, only God and I understood what I was doing.
+// Now, God only knows.
+
 // variables
 const cartBtn = document.querySelector('.cart-btn');
 const closeCartBtn = document.querySelector('.close-cart');
@@ -7,20 +10,41 @@ const cartOverlay = document.querySelector('.cart-overlay');
 const cartItems = document.querySelector('.cart-items');
 const cartTotal = document.querySelector('.cart-total');
 const cartContent = document.querySelector('.cart-content');
-const cartTotal = document.querySelector('.cart-total');
 const productsDOM = document.querySelector('.products-center');
 // cart
 let cart = [];
 
 // getting the products
 class Products {
-  getProducts() {
-    fetch()
+  async getProducts() {
+    try {
+      let result = await fetch('products.json')
+      let data = await result.json();
+      
+      let products = data.items;
+      products = products.map(item => {
+        const {title, price} = item.fields;
+        const {id} = item.sys;
+        const image = item.fields.image.fields.file.url;
+        return {title, price, id, image}
+      })
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+    
   }   
 }
 // display products 
 class UI {
-
+  displayProducts(products) {
+    let result = '';
+    products.forEach(product => {
+      result += `
+      
+      `
+    })
+  }
 }
 
 // local storage
@@ -31,4 +55,7 @@ class Storage {
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI() 
   const products = new Products();
+
+  // get all products
+  products.getProducts().then(products => ui.displayProducts(products));
 });
