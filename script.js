d752bbc2fa4df742bbc3cf22f516e508d2256edf
Rostyslav <rklyba@yahoo.com>
2020-05-06 15:59:13 +0300

    Cart value in topright corner can be updated now

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// When I wrote this, only God and I understood what I was doing.
-// Now, God only knows.
-
 // variables
 const cartBtn = document.querySelector(".cart-btn");
 const closeCartBtn = document.querySelector(".close-cart");
@@ -11,8 +8,11 @@ const cartItems = document.querySelector(".cart-items");
 const cartTotal = document.querySelector(".cart-total");
 const cartContent = document.querySelector(".cart-content");
 const productsDOM = document.querySelector(".cards");
+
 // cart
 let cart = [];
+// buttons 
+let buttonsDOM = [];
 
 // getting the products
 class Products {
@@ -61,13 +61,59 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+  
+  getBagButtons() {
+    const buttons = [...document.querySelectorAll(".bag-btn")];
+    buttonsDOM = buttons;
+    buttons.forEach(button => {
+      let id = button.dataset.id;
+      let inCart = cart.find(item => item.id === id);
+      if(inCart) {
+        button.innerText = "In Cart";
+        button.disabled = true;
+      } 
+        button.addEventListener('click', (event) => {
+          event.target.innerText = "In Cart";
+          event.target.disabled = true;
+          // get product from products
+          let cartItem = {...Storage.getProduct(id), amount: 1};
+          // add product to the cart
+          cart = [...cart, cartItem];
+          // save cart in local storage 
+          Storage.saveCart(cart);
+          // set cart values
+          this.setCartValues(cart);
+          // display cart item
+          // show the cart
+        });
+      
+    });
+  }
+  setCartValues(cart) {
+    let tempTotal = 0;
+    let itemsTotal = 0;
+    cart.map(item => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+    console.log(cartTotal, cartItems);
+  }
 }
 
 // local storage
 class Storage {
   static saveProducts(products) {
     localStorage.setItem("products", JSON.stringify(products));
+  }
+  static getProduct(id) {
+    let products = JSON.parse(localStorage.getItem('products'));
+    return products.find(product => product.id === id);
   }  
+  static saveCart(cart) {
+    localStorage.setItem('cart', JSON.stringify(cart))
+  }    
 }
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -78,5 +124,10 @@ document.addEventListener("DOMContentLoaded", () => {
   products.getProducts().then(products => {
     ui.displayProducts(products);
     Storage.saveProducts(products);
+  }).then(() => {
+   ui.getBagButtons(); 
   });
 });
+
+// When I wrote this, only God and I understood what I was doing.
+// Now, God only knows.
