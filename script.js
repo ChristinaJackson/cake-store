c653a02c10900506f11c3a9c8c408493546b557c
Rostyslav <rklyba@yahoo.com>
2020-05-06 18:17:23 +0300

    Added ability to clear the cart

--- script.js
+++ script.js
@@ -49,7 +49,7 @@ class UI {
           />
            <button class="bag-btn" data-id=${product.id}>
             <i class="fas fa-shopping-cart"></i>
-            add to bag
+            add to cart
           </button>
         </div>
         <div class="itemName">
@@ -136,6 +136,33 @@ class UI {
     cartOverlay.classList.remove('transparentBcg');
     cartDOM.classList.remove('showCart');
   }
+  cartLogic() {
+    // clear cart button
+    clearCartBtn.addEventListener('click', () => {
+      this.clearCart();
+    });
+    // cart functionality
+  }
+  clearCart() {
+    let cartItems = cart.map(item => item.id);
+    cartItems.forEach(id => this.removeItem(id));
+    console.log(cartContent.children);
+    while(cartContent.children.length > 0) {
+    cartContent.removeChild(cartContent.children[0])
+    }
+    this.hideCart();
+  }
+  removeItem(id) {
+    cart = cart.filter(item => item.id !==id);
+    this.setCartValues(cart);
+    Storage.saveCart(cart);
+    let button = this.getSingleButton(id);
+    button.disabled = false;
+    button.innerHTML = `<i class="fas fa-shopping-cart"></i>add to cart`;
+  }
+  getSingleButton(id) {
+    return buttonsDOM.find(button => button.dataset.id === id);
+  }
 }
 
 // local storage
@@ -169,6 +196,7 @@ ui.setupAPP();
     })
     .then(() => {
       ui.getBagButtons();
+      ui.cartLogic();
     });
 });
 
