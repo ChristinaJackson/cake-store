749c3776914d600f6421634955ca278b9d0d4d0b
Rostyslav <rklyba@yahoo.com>
2020-05-06 13:06:22 +0300

    Added movable cart buttons on images

--- script.js
+++ script.js
@@ -2,15 +2,15 @@
 // Now, God only knows.
 
 // variables
-const cartBtn = document.querySelector('.cart-btn');
-const closeCartBtn = document.querySelector('.close-cart');
-const clearCartBtn = document.querySelector('.clear-cart');
-const cartDOM = document.querySelector('.cart');
-const cartOverlay = document.querySelector('.cart-overlay');
-const cartItems = document.querySelector('.cart-items');
-const cartTotal = document.querySelector('.cart-total');
-const cartContent = document.querySelector('.cart-content');
-const productsDOM = document.querySelector('.cards');
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".cards");
 // cart
 let cart = [];
 
@@ -18,55 +18,58 @@ let cart = [];
 class Products {
   async getProducts() {
     try {
-      let result = await fetch('products.json')
+      let result = await fetch("products.json");
       let data = await result.json();
-      
+
       let products = data.items;
-      products = products.map(item => {
-        const {title, price} = item.fields;
-        const {id} = item.sys;
+      products = products.map((item) => {
+        const { title, price } = item.fields;
+        const { id } = item.sys;
         const image = item.fields.image.fields.file.url;
-        return {title, price, id, image}
-      })
+        return { title, price, id, image };
+      });
       return products;
     } catch (error) {
       console.log(error);
     }
-    
-  }   
+  }
 }
-// display products 
+// display products
 class UI {
   displayProducts(products) {
-    let result = '';
-    products.forEach(product => {
+    let result = "";
+    products.forEach((product) => {
       result += `
       <div class="card">
-            <img
-              class="itemImage"
-              src=${product.image}
-              alt=""
-            />
-            <div class="itemName">
-              <p>${product.title}</p>
-              <p>$${product.price}</p>
-            </div>
+        <div class="img-container">  
+          <img
+            class="itemImage"
+            src=${product.image}
+            alt=""
+          />
+           <button class="bag-btn" data-id=${product.id}>
+            <i class="fas fa-shopping-cart"></i>
+            add to bag
+          </button>
+        </div>
+        <div class="itemName">
+          <p>${product.title}</p>
+          <p>$${product.price}</p>
+        </div>
       </div>
       `;
     });
-    productsDOM.innerHTML = result; 
+    productsDOM.innerHTML = result;
   }
 }
 
 // local storage
-class Storage {
-
-}
+class Storage {}
 
 document.addEventListener("DOMContentLoaded", () => {
-  const ui = new UI() 
+  const ui = new UI();
   const products = new Products();
 
   // get all products
-  products.getProducts().then(products => ui.displayProducts(products));
+  products.getProducts().then((products) => ui.displayProducts(products));
 });
